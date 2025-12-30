ebbabcac2185dee7b49f674cad39cae61c0f84fd
fahmifath <fahmifthur@gmail.com>
2025-12-30 19:02:45 +0700

    Add routing for Task and SubTask API endpoints

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\SubTaskController;
+
+
+Route::apiResource('tasks', TaskController::class);
+
+Route::post('tasks/{taskId}/subtasks',[SubTaskController::class, 'store']);
+Route::put('subtasks/{id}',[SubTaskController::class, 'update']);
+Route::delete('subtasks/{id}',[SubTaskController::class, 'destroy']);
