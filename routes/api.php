fef304a9d6bcc0df342650544b0f36b7c9f10051
fahmifath <fahmifthur@gmail.com>
2026-01-01 23:03:36 +0700

    Remove index method from SubTaskController and corresponding route

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\SubTaskController;
 
-
 Route::apiResource('tasks', TaskController::class);
 
-Route::get('tasks/{taskId}/subtasks',[SubTaskController::class, 'index']);
 Route::post('tasks/{taskId}/subtasks',[SubTaskController::class, 'store']);
 Route::put('subtasks/{id}',[SubTaskController::class, 'update']);
 Route::delete('subtasks/{id}',[SubTaskController::class, 'destroy']);
